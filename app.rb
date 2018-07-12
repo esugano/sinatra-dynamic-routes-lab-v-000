b410575d76ec6dcfa6f1fec8accfa776e18be16a
Emily S <emily.sugano@gmail.com>
2018-07-12 22:50:35 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -25,6 +25,7 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
   op = params[:operation]
+  "#{op}"
   def math(op)
     num_1 = params[:number1].to_i
     num_2 = params[:number2].to_i
