85c98699a693846b34b362f5e6c39c770417610f
Emily S <emily.sugano@gmail.com>
2018-07-12 22:42:30 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-  "#{parms[:number1] + parms[:number2]}"
+  "#{parms[:number1].to_i + parms[:number2].to_i }"
 
   end
 
